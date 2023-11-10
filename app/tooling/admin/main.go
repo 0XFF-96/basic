229c12e6c254fac3668c38f1973272c2083cb713
jimmyLi <13420134410@163.com>
2023-11-10 17:41:08 +0800

    feat: add jwt and generate token and signature verification

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,11 +6,14 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	err := GenKey()
+	err := GenToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -71,3 +74,95 @@ func GenKey() error {
 	fmt.Println("private and public key files generated")
 	return nil
 }
+
+// GenToken generates a JWT for the specified user.
+func GenToken() error {
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := &RegisteredClaim{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "usr.ID.String()",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"usr.Roles"},
+	}
+
+	// This will generate a JWT with the claims embedded in them. The database
+	// with need to be configured with the information found in the public key
+	// file to validate these claims. Dgraph does not support key rotate at
+	// this time.
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1" // rotate the key, private key got leaked~
+
+	name := "zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem"
+	file, err := os.Open(name)
+
+	// limit PEM file size to 1 megabyte. This should be reasonable for
+	// almost any PEM file and prevents shenanigans like linking the file
+	// to /dev/random or something like that.
+	pemFile, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	pk, err := jwt.ParseRSAPrivateKeyFromPEM(pemFile)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	str, err := token.SignedString(pk)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("============== TOKEN ===============")
+	fmt.Println(str)
+	fmt.Println("============== TOKEN ===============")
+	fmt.Print("\n")
+
+	//token, err := a.GenerateToken(kid, claims)
+	//if err != nil {
+	//	return fmt.Errorf("generating token: %w", err)
+	//}
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&pk.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	// Write the public key to the public key file.
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	return nil
+}
+
+type RegisteredClaim struct {
+	RegisteredClaims jwt.RegisteredClaims
+	Roles            []string
+}
+
+// 1. 如果是 embeded the claim,
+// 2. 那么接口的逻辑就会自动实现了
+func (r *RegisteredClaim) Valid() error { return nil }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/ardanlabs/conf/v3 v3.1.7
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.4.0
 	github.com/jmoiron/sqlx v1.3.5
 	go.opentelemetry.io/otel v1.19.0
