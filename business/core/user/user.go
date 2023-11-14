79373238805a95f5ca5e0241a6cd52106b7583c4
jimmyLi <13420134410@163.com>
2023-11-14 10:12:37 +0800

    feat: add model filter

---
+++ business/core/user/user.go
@@ -0,0 +1,208 @@
+// Package user provides an example of a core business API. Right now these
+// calls are just wrapping the data/data layer. But at some point you will
+// want auditing or something that isn't specific to the data/store layer.
+package user
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/yourusername/basic-a/business/data/order"
+	"github.com/yourusername/basic-a/business/sys/validate"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrNotFound              = errors.New("user not found")
+	ErrInvalidID             = errors.New("ID is not in its proper form")
+	ErrInvalidEmail          = errors.New("email is not valid")
+	ErrUniqueEmail           = errors.New("email is not unique")
+	ErrAuthenticationFailure = errors.New("authentication failed")
+	ErrInvalidOrder          = errors.New("validating order by")
+)
+
+// Storer interface declares the behavior this package needs to perists and
+// retrieve data.
+type Storer interface {
+	WithinTran(ctx context.Context, fn func(s Storer) error) error
+	Create(ctx context.Context, usr User) error
+	Update(ctx context.Context, usr User) error
+	Delete(ctx context.Context, userID string) error
+	Query(ctx context.Context, filter QueryFilter, orderBy order.By, pageNumber int, rowsPerPage int) ([]User, error)
+	QueryByID(ctx context.Context, userID string) (User, error)
+	QueryByEmail(ctx context.Context, email string) (User, error)
+}
+
+// Core manages the set of APIs for user access.
+type Core struct {
+	storer Storer
+}
+
+// NewCore constructs a core for user api access.
+func NewCore(storer Storer) *Core {
+	return &Core{
+		storer: storer,
+	}
+}
+
+// Create inserts a new user into the database.
+func (c *Core) Create(ctx context.Context, nu NewUser) (User, error) {
+	if err := validate.Check(nu); err != nil {
+		return User{}, fmt.Errorf("validating data: %w", err)
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, fmt.Errorf("generating password hash: %w", err)
+	}
+
+	now := time.Now().UTC()
+
+	usr := User{
+		ID:           validate.GenerateID(),
+		Name:         nu.Name,
+		Email:        nu.Email,
+		PasswordHash: hash,
+		Roles:        nu.Roles,
+		Enabled:      true,
+		DateCreated:  now,
+		DateUpdated:  now,
+	}
+
+	// This provides an example of how to execute a transaction if required.
+	tran := func(s Storer) error {
+		if err := s.Create(ctx, usr); err != nil {
+			return fmt.Errorf("create: %w", err)
+		}
+		return nil
+	}
+
+	if err := c.storer.WithinTran(ctx, tran); err != nil {
+		return User{}, fmt.Errorf("tran: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Update replaces a user document in the database.
+func (c *Core) Update(ctx context.Context, userID string, uu UpdateUser) error {
+	if err := validate.CheckID(userID); err != nil {
+		return ErrInvalidID
+	}
+
+	if err := validate.Check(uu); err != nil {
+		return fmt.Errorf("validating data: %w", err)
+	}
+
+	user, err := c.storer.QueryByID(ctx, userID)
+	if err != nil {
+		return fmt.Errorf("updating user userID[%s]: %w", userID, err)
+	}
+
+	if uu.Name != nil {
+		user.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		user.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		user.Roles = uu.Roles
+	}
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Errorf("generating password hash: %w", err)
+		}
+		user.PasswordHash = pw
+	}
+	if uu.Enabled != nil {
+		user.Enabled = *uu.Enabled
+	}
+	user.DateUpdated = time.Now().UTC()
+
+	if err := c.storer.Update(ctx, user); err != nil {
+		return fmt.Errorf("update: %w", err)
+	}
+
+	return nil
+}
+
+// Delete removes a user from the database.
+func (c *Core) Delete(ctx context.Context, userID string) error {
+	if err := validate.CheckID(userID); err != nil {
+		return ErrInvalidID
+	}
+
+	if err := c.storer.Delete(ctx, userID); err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+
+	return nil
+}
+
+// Query retrieves a list of existing users from the database.
+func (c *Core) Query(ctx context.Context, filter QueryFilter, orderBy order.By, pageNumber int, rowsPerPage int) ([]User, error) {
+	if err := validate.Check(filter); err != nil {
+		return nil, fmt.Errorf("validating filter: %w", err)
+	}
+
+	if err := ordering.Check(orderBy); err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidOrder, err.Error())
+	}
+
+	users, err := c.storer.Query(ctx, filter, orderBy, pageNumber, rowsPerPage)
+	if err != nil {
+		return nil, fmt.Errorf("query: %w", err)
+	}
+
+	return users, nil
+}
+
+// QueryByID gets the specified user from the database.
+func (c *Core) QueryByID(ctx context.Context, userID string) (User, error) {
+	if err := validate.CheckID(userID); err != nil {
+		return User{}, ErrInvalidID
+	}
+
+	user, err := c.storer.QueryByID(ctx, userID)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
+}
+
+// QueryByEmail gets the specified user from the database by email.
+func (c *Core) QueryByEmail(ctx context.Context, email string) (User, error) {
+
+	// Email Validate function in validate.
+	if !validate.CheckEmail(email) {
+		return User{}, ErrInvalidEmail
+	}
+
+	user, err := c.storer.QueryByEmail(ctx, email)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	return user, nil
+}
+
+// Authenticate finds a user by their email and verifies their password. On
+// success it returns a Claims User representing this user. The claims can be
+// used to generate a token for future authentication.
+func (c *Core) Authenticate(ctx context.Context, email, password string) (User, error) {
+	usr, err := c.storer.QueryByEmail(ctx, email)
+	if err != nil {
+		return User{}, fmt.Errorf("query: %w", err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(usr.PasswordHash, []byte(password)); err != nil {
+		return User{}, ErrAuthenticationFailure
+	}
+
+	return usr, nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/ardanlabs/conf/v3 v3.1.7
 	github.com/ardanlabs/darwin v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/go-playground/locales v0.14.1
+	github.com/go-playground/universal-translator v0.18.1
+	github.com/go-playground/validator/v10 v10.16.0
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.4.0
 	github.com/jmoiron/sqlx v1.3.5
@@ -14,6 +17,14 @@ require (
 	go.opentelemetry.io/otel/trace v1.19.0
 	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.26.0
+	golang.org/x/crypto v0.15.0
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
